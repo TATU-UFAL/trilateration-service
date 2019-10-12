e6621e2996a1e1b41307576d16659d180f07d41f
Dayvson Sales <dayvson@outlook.com>
2019-10-11 21:53:06 -0300

    add options

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -14,9 +14,24 @@ public class App {
     public static void main(String args[]){
         port(4000);
 
-        after((Filter) (request, response) -> {
-            response.header("Access-Control-Allow-Origin", "*");
-            response.header("Access-Control-Allow-Methods", "GET");
+        options("/*", (req, res) -> {
+            String accessControlRequestHeaders = req.headers("Access-Control-Request-Headers");
+            if (accessControlRequestHeaders != null) {
+                res.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
+            }
+
+            String accessControlRequestMethod = req.headers("Access-Control-Request-Method");
+            if (accessControlRequestMethod != null) {
+                res.header("Access-Control-Allow-Methods", accessControlRequestMethod);
+            }
+
+            return "OK";
+        });
+
+        before((req, res) -> {
+            res.header("Access-Control-Allow-Origin", "*");
+            res.header("Access-Control-Allow-Headers", "*");
+            res.type("application/json");
         });
 
         post("/trilateration", (req, res) -> {
